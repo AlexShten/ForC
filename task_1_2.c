3f53a31fd1912189c806a538306bdeea9dcded29
Alexander_Shtenhelov <shtengelov.aleks@gmail.com>
2021-08-20 01:17:51 +0300

    HW1 indent

--- task_1_2.c
+++ task_1_2.c
@@ -1,54 +1,54 @@
 #include <stdio.h>
 
 struct RGB8{
-		unsigned char R;
-		unsigned char G;
-		unsigned char B;
+	unsigned char R;
+	unsigned char G;
+	unsigned char B;
 };
 
 struct RGB5{
-		unsigned char R:5;
-		unsigned char G1:3;
-		unsigned char G2:3;
-		unsigned char B:5;
+	unsigned char R:5;
+	unsigned char G1:3;
+	unsigned char G2:3;
+	unsigned char B:5;
 };
 
 struct RGB5 convert(struct RGB8);
 
 int main(){
-		
-		struct RGB8 rgb888;
-	
-		printf("Please enter value of red channel\n");
-		scanf("%hhd", &rgb888.R);
-		printf("Please enter value of green channel\n");
-		scanf("%hhd", &rgb888.G);
-		printf("Please enter value of blue channel\n");
-		scanf("%hhd", &rgb888.B);
-	
-		struct RGB5 rgb565 = convert(rgb888);
-		
-		printf("%d.%d.%d\n", rgb888.R, rgb888.G, rgb888.B);
-		printf("%d.%d.%d\n", rgb565.R, rgb565.G1 + rgb565.G2 * 8, rgb565.B);	
-	
+
+	struct RGB8 rgb888;
+
+	printf("Please enter value of red channel\n");
+	scanf("%hhd", &rgb888.R);
+	printf("Please enter value of green channel\n");
+	scanf("%hhd", &rgb888.G);
+	printf("Please enter value of blue channel\n");
+	scanf("%hhd", &rgb888.B);
+
+	struct RGB5 rgb565 = convert(rgb888);
+
+	printf("%d.%d.%d\n", rgb888.R, rgb888.G, rgb888.B);
+	printf("%d.%d.%d\n", rgb565.R, rgb565.G1 + rgb565.G2 * 8, rgb565.B);	
+
 }
 
 struct RGB5 convert(struct RGB8 rgb)
 {
-		struct RGB5 newRGB;
-		char G;
-	
-		newRGB.R = (char)((float) rgb.R * 31.0 / 255.0 + 0.5 );		
-		newRGB.B = (char)((float) rgb.B * 31.0 / 255.0 + 0.5 );
-		G = (char)((float) rgb.G * 63.0 / 255.0 + 0.5 );
-	
-		if (G <= 7) {
-				newRGB.G1 = G;
-				newRGB.G2 = 0;
-		} else {
-				newRGB.G1 = G % 8;
-				newRGB.G2 = G / 8;
-		}
-	
-		return newRGB;
+	struct RGB5 newRGB;
+	char G;
+
+	newRGB.R = (char)((float) rgb.R * 31.0 / 255.0 + 0.5 );		
+	newRGB.B = (char)((float) rgb.B * 31.0 / 255.0 + 0.5 );
+	G = (char)((float) rgb.G * 63.0 / 255.0 + 0.5 );
+
+	if (G <= 7) {
+		newRGB.G1 = G;
+		newRGB.G2 = 0;
+	} else {
+		newRGB.G1 = G % 8;
+		newRGB.G2 = G / 8;
+	}
+
+	return newRGB;
 }
--- task_1_5.c
+++ task_1_5.c
@@ -2,13 +2,13 @@
 
 int main(){
 
-		const char val = 10;
-		long fact = 1;
+	const char val = 10;
+	long fact = 1;
 
-		for (char x = 1; x <= val; x++){
-				fact *= x;	
-		}
-	
-		printf("Factorial of 10 is:\n");
-		printf("%ld\n", fact);
+	for (char x = 1; x <= val; x++){
+		fact *= x;	
+	}
+
+	printf("Factorial of 10 is:\n");
+	printf("%ld\n", fact);
 }
